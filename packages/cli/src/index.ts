2ca23b74cea5ebceb85166112f4a1285f819730a
mrozio13pl <mrozio13pl@gmail.com>
2026-03-16 22:08:58 +0100

    fix: use available port

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import opener from 'opener';
+import getPort, { portNumbers } from 'get-port';
+
+const freePort = await getPort({
+    port: [3600, ...portNumbers(36_000, 46_000)],
+});
+process.env.PORT = freePort.toString();
 
 // @ts-expect-error imported at build-time
 await import('./web/server/index.mjs');
 
-opener('http://localhost:3000');
+opener(`http://127.0.0.1:${freePort}`);
